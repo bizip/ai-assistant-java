a20cc874903ce4609fb5296fa8e4cd19ea7445d8
bizip <bizip04@gmail.com>
2023-04-24 12:03:09 +0200

    Add api key inside env

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,11 +1,14 @@
 package com.main;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Properties;
+
 import javax.swing.JOptionPane;
 
 import org.json.JSONException;
@@ -14,7 +17,10 @@ import org.json.JSONObject;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
+    	 Properties props = new Properties();
+         try (FileInputStream fis = new FileInputStream("config.properties")) {
+             props.load(fis);
+         }
         String apiUrl = "https://api.openai.com/v1/chat/completions";
         String statement = JOptionPane.showInputDialog(null, "Please ask your AI assistant:");
         String requestBody = "{"
@@ -22,12 +28,13 @@ public class Main {
                 + "\"messages\": [{\"role\": \"user\", \"content\": \"" + statement + "\"}],"
                 + "\"temperature\": 0.7"
                 + "}";
-
+       
+        String apiKey = props.getProperty("api.key");
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(apiUrl))
                 .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer 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")
+                .header("Authorization", "Bearer " + apiKey)
                 .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                 .build();
 
